13fd23d6aa602e8160c8a7ca271b46e2c313b928
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-07-22 19:36:33 -0400

    Sets the files structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
-
-const App = () => {
+import React from 'react';
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from "./containers";
+import {CTA, Brand, Navbar} from "./components";
+import "./App.css"
+  
+function App(){
   return (
-    <div>App</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
